3bc6e8264c74e5555758a492064fc68e72a71287
Walace Paisano <58262296+wpaisano@users.noreply.github.com>
2023-05-12 05:45:33 -0300

    desafio010

--- Desafios/010.py
+++ Desafios/010.py
@@ -1,2 +1,6 @@
 #Crie um programa que leia quanto dinheiro a pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 
+c=float(input('Informe quanto dinheiro você tem na carteira: '))
+d=4.93
+conv=c/d
+print('Você pode comprar {}$'.fomart(conv))
