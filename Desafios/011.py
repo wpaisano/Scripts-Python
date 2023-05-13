8af155b4a4b5ab4e6c9e870356db5528675d33b7
Walace Paisano <walacepaisano@gmail.com>
2023-05-13 06:50:36 -0300

    atualização

--- Desafios/011.py
+++ Desafios/011.py
@@ -1,2 +1,11 @@
 # Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade de tinra necessária para pintá-la, sabendo que cada 
 # metro de tinta, pinta uma área de 2m.
+
+largura=float(input('Informe a largura da parede: '))
+altura=float(input('Informe a altura da parede: '))
+area=(largura*altura)
+qtdetinta=area/2
+print('A parede tem uma área de {} e serão necessários {} para pintar toda a parede.'.format(area,qtdetinta))
+
+
+
