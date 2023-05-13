5df698b990e0c034d538d5f8925b49afd50da03b
Walace Paisano <walacepaisano@gmail.com>
2023-05-13 07:00:11 -0300

    atualização

--- Desafios/012.py
+++ Desafios/012.py
@@ -1,1 +1,6 @@
 # Faça um algoritmo que leia o preço de um produto e mostre seu novo preço com 5% de desconto.
+
+prd=float(input('Informe o preço do produto: '))
+desc=0.05
+novopreco=prd-(prd*desc)
+print('O valor do produto com desconto é: {}.'.format(novopreco))
--- Desafios/013.py
+++ Desafios/013.py
@@ -1,1 +1,5 @@
 # Faça um algoritmo que leia o salário de um funcionário e mostre o seu novo salário com 15% de aumento.
+
+sal=float(input('Informe o salário do funcionário: '))
+reajuste=sal+(sal*0.15)
+print('O novo salário do funcionário é: {}'.format(reajuste))
