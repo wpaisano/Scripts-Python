6d9930aa5f68e321e245b1d5f15258ab35e5dc63
Walace Paisano <walacepaisano@gmail.com>
2023-05-15 12:18:51 -0300

    atualizaçao

---
+++ Aulas/aula08.py
@@ -0,0 +1,22 @@
+
+'''num=int(input('Digite um número: '))
+raiz=math.sqrt(num)
+print('A raiz de {} é igual a {}'.format(num, raiz))
+
+from math import sqrt
+from math import floor
+
+num=int(input('Digite um número: '))
+raiz=sqrt(num)
+print('A raiz de {} é igual a {:.2f}'.format(num, floor(raiz)))'''
+
+import random
+num=random.random()
+print(num)
+
+import random
+num=random.randint(1, 10)
+print(num)
+ 
+
+
