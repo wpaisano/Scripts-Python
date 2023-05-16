d1c8a7a93c52ff296edb6681b7cc370e500648b9
Walace Paisano <58262296+wpaisano@users.noreply.github.com>
2023-05-16 14:05:33 -0300

    Delete Exercícios directory

--- Exercícios/ex003.py
+++
@@ -1,7 +0,0 @@
-#Faça um progrma que leia o nome de uma pessoa e mostre uma mensagem de boas vindas.
-
-nome=input('Digite o nome: ')
-print('E um prazer te conhecer, {}!'.format(nome))
- 
-
- console.log('olA MUNNDO')
