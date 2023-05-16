129fd6304feac4e7cce87534602a1bd8caedf4eb
Walace Paisano <58262296+wpaisano@users.noreply.github.com>
2023-05-16 14:04:53 -0300

    Delete MeusExercícios directory

--- MeusExercícios/aula06a.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input('Digite o valor: '))
-n2 = int(input('Digite outro valor: '))
-s = n1 + n2
-print('A soma valor', s)
-print('A soma entre {} e {} vale {}'.format(n1, n2, s))
--- MeusExercícios/aula06b.py
+++
@@ -1,1 +0,0 @@
-n = str(input('Digite um valor: ')
