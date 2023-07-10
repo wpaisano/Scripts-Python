57b8ba99fd147588610be35ef77dc5cb6169bcb1
Walace Paisano <walacepaisano@gmail.com>
2023-07-10 17:24:55 -0300

    Atualizadas as soluções dos desafios 016, 017, 018, 019, 020 e 021

--- Desafios/019.py
+++ Desafios/019.py
@@ -1,1 +1,10 @@
-'''Um professos quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido'''
+'''Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido'''
+import random
+aluno=str(input('Informe o primero aluno: '))
+aluno1=str(input('Informe o segundo aluno: '))
+aluno2=str(input('Informe o terceiro aluno: '))
+aluno3=str(input('Informe o quarto aluno: '))
+lista=[aluno, aluno1, aluno2, aluno3]
+print('O aluno sorteado foi: ',random.choice(lista))
+#print('O aluno embaralhado foi: ',random.shuffle(lista))
+#print('O aluno sorteado no intervalo foi: ',random.randint(aluno, aluno3))
